6e897d2bba8157ffc34adc328c15fbc92e1edd06
Martin Biolek <43006943+mbio16@users.noreply.github.com>
2020-10-14 12:24:06 +0200

    Main window - protocol select

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/ui/Sample.fxml")));
+			Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/ui/Main.fxml")));
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
---
+++ src/ui/MainController.java
@@ -0,0 +1,14 @@
+package ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+
+public class MainController {
+	
+	@FXML
+	private RadioButton czechLangRadioButton, englishLangRadioButton;
+	
+	@FXML
+	private Button dnsButton, llmrButton,mdnsButton,dohButton,dotButton;
+}
